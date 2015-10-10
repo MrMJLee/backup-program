992baace8589d5debf03bb2b4a8a318c3f4c5648
MJ Lee <itmjlee@gmail.com>
2015-10-10 21:01:21 +1300

    wrote statements to check if the names of objects that don't match their filename.

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import pickle
 import os
 import os.path
+import hashlib
+import shutil
 from directories import Directories
 from init import init
+from store import *
+
+
 
 def test():
     d = Directories()
@@ -21,7 +26,7 @@ def test():
         filenames = os.listdir(d.objects)
         sizeofIndex = len(indices)
         sizeofFiles = len(filenames)
-
+        existFiles = {}
         errorPath = []
         if sizeofFiles> 0 or sizeofIndex>0:
             print "The size of index %d" % sizeofIndex
@@ -29,21 +34,34 @@ def test():
             for n in indices:
                 if indices.get(n) in filenames:
                     filenames.remove(indices.get(n))
+                    existFiles[n] = indices.get(n)
                     count+=1
                 else:
                     errorPath.append(n)
-            print "Matching filenames: %d" %count
+            print "Matching filenames: %d\n" %count
             print "Names of erroneous paths: "
-            print '\n'.join(errorPath)
+            print '\n'.join(errorPath) + '\n'
 
-    else:
-        print "Index file doesn't exist"
-        person = raw_input('Would you like to create the index file?: (y/n) ')
-        if(person.lower() == 'y'):
-            init()
-            test()
+        else:
+            print "Index file doesn't exist"
+            person = raw_input('Would you like to create the index file?: (y/n) ')
+            if(person.lower() == 'y'):
+                init()
+                test()
 
-    print "\n"
+        contentsWrong = []
+        for file in existFiles:
+            sig= createFileSignature(file)
+            newhashvalue = sig[2]
+            if newhashvalue not in existFiles.values():
+                contentsWrong.append(sig[0])
+
+        if len(contentsWrong) !=0:
+            doesntmatch = [x[0] for x in contentsWrong]
+            print "the names of objects that don't match their filename: "
+            print '\n'.join(doesntmatch)
+        else:
+            print "All names of objects match their filename"
 
 
 
