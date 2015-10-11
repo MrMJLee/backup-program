120f8127706c1b8c987353b08d7021f02c236905
Joel Waldock <waldock@windowslive.com>
2015-10-11 16:18:11 +1300

    modify test methods

--- test.py
+++ test.py
@@ -1,70 +1,56 @@
-import pickle
 import os
 import os.path
-import hashlib
-import shutil
-from directories import Directories
-from init import init
+
 from store import *
 
+d = Directories()
 
 
 def test():
-    d = Directories()
-    count = 0;
+    test_index()
+    print
+    contentsMatch()
+
+
+def test_index():
+    print "Checking Index:"
     if os.path.exists(d.index) and os.path.isfile(d.index):
         # storing current hash values in a index file as a list
         indices = d.paths_in_index()
         # storing current filenames in the object folder as a list
         filenames = os.listdir(d.objects)
-        sizeofIndex = len(indices)
-        sizeofFiles = len(filenames)
-        # matching files
-        existFiles = {}
-        # the names of any erroneous paths that don't have matching files
-        errorPath = []
-        if sizeofFiles> 0 or sizeofIndex>0:
-            print "The size of index %d" % sizeofIndex
-            print "The size of files in object folder %d" % sizeofFiles
-            for n in indices:
-                if indices.get(n) in filenames:
-                    filenames.remove(indices.get(n))
-                    existFiles[n] = indices.get(n)
-                    count+=1
-                else:
-                    errorPath.append(n)
-            print "Matching filenames: %d\n" %count
-            print "Names of erroneous paths: "
-            print '\n'.join(errorPath) + '\n'
 
+        matching_paths = 0
+        errors = 0
+
+        for n in indices:
+            if indices.get(n) in filenames:
+                matching_paths += 1
+            else:
+                print "File does not exist: % s" % n
+                errors += 1
+
+        print "%d Matching files" % matching_paths
+        print "%d Errors" % errors
+    else:
+        print "Index file doesn't exist"
+
+
+def contentsMatch():
+        # print hash_vals
+        print "Checking File Hash Values"
+        num_wrong = 0
+
+        for path in os.listdir(d.objects):
+            fullpath = os.path.join(d.objects, path)
+            if os.path.isfile(fullpath):
+                actual_hash = createFileSignature(fullpath)[2]
+
+                if path != actual_hash:
+                    print "Error file: %s" % fullpath
+                    num_wrong+=1
+
+        if num_wrong !=0:
+            print "%d file(s) incorrect" % num_wrong
         else:
-            print "Index file doesn't exist"
-            person = raw_input('Would you like to create the index file?: (y/n) ')
-            if(person.lower() == 'y'):
-                init()
-                test()
-    contentsMatch(existFiles)
-
-
-def contentsMatch(existFiles):
-        contentsWrong = []
-        for file in existFiles:
-            if os.path.isfile(file):
-                sig= createFileSignature(file)
-                newhashvalue = sig[2]
-                if newhashvalue not in existFiles.values():
-                    contentsWrong.append(sig[0])
-            if os.path.isdir(file):
-                contentsMatch(file)
-
-        if len(contentsWrong) !=0:
-            doesntmatch = [x[0] for x in contentsWrong]
-            print "the names of objects that don't match their filename: "
-            print '\n'.join(doesntmatch)
-        else:
-            print "All names of objects match their filename"
-
-
-
-
-
+            print "All hash values correct"
