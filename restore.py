6303d08172eafacd62f5f30547adbc9a1f267388
MJ Lee <itmjlee@gmail.com>
2015-10-12 22:29:35 +1300

    update restore.py

--- restore.py
+++ restore.py
@@ -1,37 +1,35 @@
 import os
 import shutil
 from directories import Directories
+
 def restore(destPath=None):
     d = Directories()
     paths = d.paths_in_index()
-    options = []
 
-    for x in paths:
-        options.append(x)
-
-    for i in range(0,len(options)):
-        path =  options[i]
+    for path in paths.keys():
         try:
             sourcePath = os.path.join(d.objects, paths[path])
-            copyTo = os.path.split(path)[-1]
+           # print sourcePath
             if destPath is None:
-                copyFiles(path,sourcePath,copyTo)
+                dest = path
             else:
-                dest = os.path.join(destPath,copyTo)
-                copyFiles(path,sourcePath,dest)
+                dest = os.path.join(destPath, path)
+            directory = os.path.join(*os.path.split(dest)[0:-1])
+            restoreFiles(dest,sourcePath,directory)
         except IOError:
             print "Invalid Path"
             break
 
-def copyFiles(path,sourcePath,copyTo):
-    if os.path.exists(copyTo):
-        ask = raw_input("%s already exists, Overwrite? (y/n) " %path)
+def restoreFiles(dest, sourcePath,directory):
+    if not os.path.exists(directory) and not os.path.isdir(directory):
+        os.makedirs(directory) # make directory
+    if os.path.exists(dest):
+        ask = raw_input("%s already exists, Overwrite? (y/n) " %dest)
         if ask.lower() == 'y':
-            shutil.copyfile(sourcePath ,copyTo)
-            print path+ " saved successfully\n"
-
+            shutil.copyfile(sourcePath, dest)
+            print dest+ " saved successfully\n"
+        else:
+            print
     else:
-        shutil.copyfile(sourcePath ,copyTo)
-        print path+ " saved successfully\n"
-
-
+        shutil.copyfile(sourcePath ,dest)
+        print dest+ " saved successfully\n"
